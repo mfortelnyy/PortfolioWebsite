29c9973488697d27c619d33b8c1d7507b141085e
mfortelnyy <mfortelnyy@hawk.iit.edu>
2025-02-10 22:46:35 -0600

    Inject Cookies

--- Program.cs
+++ Program.cs
@@ -1,7 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using PortfolioWebsite.Models;
+using  PortfolioWebsite.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add session services
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // Session timeout
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true; //GDPR compliance
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); 
+
+builder.Services.AddScoped<VisitorLogService>();
 
 var app = builder.Build();
 
@@ -17,6 +35,9 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+// Enable session
+app.UseSession();
 
 app.UseAuthorization();
 
