e694282ea1a08d8b02e8cf424a3c90e727ac0291
mfortelnyy <mfortelnyy@hawk.iit.edu>
2025-02-17 20:51:46 -0600

    Improve project cards styling

--- obj/Debug/net8.0/PortfolioWebsite.AssemblyInfo.cs
+++ obj/Debug/net8.0/PortfolioWebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e551907a145c4ca4c8d1e5f79ce88e8d054d5c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8137430a1ac57f8661154cb270f891cc2b36e3fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
